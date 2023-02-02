577f4d8767a578b41ce8a08abbb2d764ecd1eff8
esin87 <esin.saribudak@gmail.com>
2023-02-01 20:45:57 -0600

    Finish adding sort exercises

--- exercises/03-sort.js
+++ exercises/03-sort.js
@@ -1,6 +1,8 @@
 const states = require('../data/states.json');
 const art = require('../data/art.json');
 
-// 1. Sort the states array in ascending order of latitude.
+// 1. Sort the states array in descending order of capital names (Z - A).
 
-// 2. 
+// 2. Sort the states array in ascending order of latitude.
+
+// 3. Sort the art array in ascending order of creation (use date_start).
